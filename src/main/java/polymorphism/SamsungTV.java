2c6d530f4781c138ac3da8298967676fa6eebc4f
jklee <28668509+9033@users.noreply.github.com>
2018-12-09 21:30:24 +0900

    오타및 주석 추가
    SamsungTV.java
    applicationContext.xml

--- src/main/java/polymorphism/SamsungTV.java
+++ src/main/java/polymorphism/SamsungTV.java
@@ -4,7 +4,7 @@ public class SamsungTV implements TV{
 	private Speaker speaker;
 	private int price;
 	
-	//setter injection
+	//setter injection : property 엘리먼트를 사용하거나 p namespace사용
 	public void setSpeaker(Speaker speaker) {
 		System.out.println("setSpeaker 호출");
 		this.speaker = speaker;
@@ -14,24 +14,26 @@ public class SamsungTV implements TV{
 		this.price = price;
 	}
 	
-	public SamsungTV(){
+	public SamsungTV(){//생성자
 		System.out.println("SamsungTV 생성");
 	}
-	public SamsungTV(Speaker speaker){
+	public SamsungTV(Speaker speaker){//constructor injection : constructor-arg엘리먼트 사용
 		System.out.println("SamsungTV 생성(Speaker)");
 		this.speaker=speaker;
 	}
-	public SamsungTV(Speaker speaker, int price){
+	public SamsungTV(Speaker speaker, int price){//constructor injection - mapping
 		System.out.println("SamsungTV 생성 (생성자 인젝션) price : "+price);
 		this.speaker=speaker;
 		this.price=price;
 	}
-	public void initMethod(){
+	public void initMethod(){//bean 엘리먼트 속성 - init-method 속성 : 객체를 생성한 이후에 호출
 		System.out.println("SamsungTV initMethod");
 	}
-	public void destoryMethod(){
-		System.out.println("SamsungTV destorytMethod");
+	public void destroyMethod(){//bean 엘리먼트 속성 - destroy-method 속성 : 객체를 생성한 이후에 호출
+		System.out.println("SamsungTV destroytMethod");
 	}
+	
+	//TV interface에 맞는 메소드
 	public void turnOn(){
 		System.out.println("SamsungTV 전원 켠다. 가격 : "+price);
 	}
