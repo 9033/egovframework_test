6fac12ef4447855ae350fb0ffa5c0836535df36b
jklee <28668509+9033@users.noreply.github.com>
2018-12-09 17:09:37 +0900

    의존성 관계, 생성자 인젝션, 다중변수 매핑
    SonySpeaker.java : 새로운 의존성 관계 추가
    polymorphism.cld : 새로운 의존성 관계 추가
    SamsungTV.java : speaker, price를 입력으로 받는 생성자 추가
    applicationContext.xml : SonySpeaker를 bean에 추가, 생성자 인젝션, 다중변수 매핑

--- src/main/java/polymorphism/SamsungTV.java
+++ src/main/java/polymorphism/SamsungTV.java
@@ -1,8 +1,16 @@
 package polymorphism;
 
 public class SamsungTV implements TV{
+	private SonySpeaker speaker;
+	private int price;
+	
 	public SamsungTV(){
 		System.out.println("SamsungTV 생성");
+	}
+	public SamsungTV(SonySpeaker speaker, int price){
+		System.out.println("SamsungTV 생성 (생성자 인젝션)");
+		this.speaker=speaker;
+		this.price=price;
 	}
 	public void initMethod(){
 		System.out.println("SamsungTV initMethod");
@@ -11,15 +19,17 @@ public class SamsungTV implements TV{
 		System.out.println("SamsungTV destorytMethod");
 	}
 	public void turnOn(){
-		System.out.println("SamsungTV 전원 켠다.");
+		System.out.println("SamsungTV 전원 켠다. 가격 : "+price);
 	}
 	public void turnOff(){
 		System.out.println("SamsungTV 전원 끈다.");
 	}
 	public void soundUp(){
-		System.out.println("SamsungTV 소리 올린다.");
+		//speaker =new SonySpeaker();
+		speaker.soundUp();
 	}
 	public void soundDown(){
-		System.out.println("SamsungTV 소리 내린다.");
+		//speaker =new SonySpeaker();
+		speaker.soundDown();
 	}	
 }
---
+++ src/main/java/polymorphism/SonySpeaker.java
@@ -0,0 +1,13 @@
+package polymorphism;
+
+public class SonySpeaker {
+	public SonySpeaker(){
+		System.out.println("SonySpeaker 생성");
+	}
+	public void soundUp(){
+		System.out.println("SonySpeaker 소리 올린다");
+	}
+	public void soundDown(){
+		System.out.println("SonySpeaker 소리 내린다");
+	}
+}
